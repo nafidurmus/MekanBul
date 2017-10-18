d71291f11170cb8fe857bf3a4897a15ce20c7637
nafi durmus <nafidurmus07@gmail.com>
2017-10-18 11:03:29 +0300

    18 ekim bitti

--- app.js
+++ app.js
@@ -38,9 +38,9 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // render the hata page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('hata');
 });
 
 module.exports = app;
--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,9 +1,97 @@
 const anaSayfa=function(req,res){
-	res.render('mekanlar-liste',{'title':'Anasayfa'});
+	res.render('mekanlar-liste',{
+		'baslik':'Anasayfa',
+		'sayfaBaslik': {
+			'siteAd':'MekanBul',
+			'aciklama':'Yakınınınzdaki meknaları görün !'
+		},
+		'mekanlar': [
+		 {
+			'ad': 'Starbucks',
+			'adres': 'Centrum Garden',
+			'puan': 4,
+			'imkanlar': ['Kahve','Pasta','Kek','Türk Kahvesi'],
+			'mesafe': '11km'
+
+		  },
+		  {
+			'ad': 'Gloria Jeans',
+			'adres': 'Iyas',
+			'puan': 3,
+			'imkanlar': ['Kahve','Latte','Kek','Türk Kahvesi'],
+			'mesafe': '7km'
+
+		  },
+		  {
+			'ad': 'Old Bear Cafe',
+			'adres': 'Kafeler Caddesi',
+			'puan': 2,
+			'imkanlar': ['Kahve','Kek'],
+			'mesafe': '14km'
+
+		  },
+		  {
+			'ad': 'Cafe Sessiz',
+			'adres': 'Kafeler Caddesi',
+			'puan': 2,
+			'imkanlar': ['Cay','Duble Cay'],
+			'mesafe': '14km'
+
+		  }
+
+
+
+		]
+
+
+	});
+
 }
 
 const mekanBilgisi=function(req,res){
-	res.render('mekan-detay',{'title':'Mekan Bilgisi'});
+	res.render('mekan-detay',{
+		'baslik':'Mekan Bilgisi',
+		'sayfaBaslik': 'Starbucks',
+		'mekanBilgisi': {
+			'ad': 'Starbucks komşum',
+			'adres': 'Centrum Garden',
+			'puan': 4,
+			'imkanlar': ['Kahve','Pasta','Kek','Türk Kahvesi'],
+			'kordinatlar': {
+				'enlem': '37.781885',
+				'boylam': '30.566034'
+			},
+			'saatler': [
+			    {
+			    	'günler':'Pazartesi-Cuma',
+			    	'acilis':'7:00',
+			    	'kapanis':'24:00',
+			    	'kapali':false
+			    },
+
+			    {
+			    	'günler':'Cumartesi',
+			    	'acilis':'7:00',
+			    	'kapanis':'24:00',
+			    	'kapali':false
+			    },
+			    {
+			    	'günler':'Pazar',
+			    	'kapali':true
+			    }
+
+			],
+			'yorumlar': [
+			     {
+			     	'yorumYapan': 'Nafi Durmus',
+			     	'puan': 3,
+			     	'tarih': '18 Ekim 2017',
+			     	'yorumMetni': 'kahvesi çok iyi yaa :)'
+			     }
+			]
+		}
+
+	});
 }
 
 const yorumEkle=function(req,res){
