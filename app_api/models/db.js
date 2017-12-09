af1e671cb9a4af6567be81a0062384578e04464b
nafi durmus <nafidurmus07@gmail.com>
2017-12-10 01:14:53 +0300

    güncellemeler yapıldı

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,42 +1,51 @@
 var mongoose = require( 'mongoose' );
-//var dbURI = 'mongodb://mekanbul:mekanbul@ds227865.mlab.com:27865/mekanbul';
-var dbURI = 'mongodb://localhost/mekanbul';
-mongoose.connect(dbURI, { 'useMongoClient': true }); 
-//Bağlandığında konsola bağlantı bilgilerini yazdır.
-mongoose.connection.on('connected', function () {
-  console.log('Mongoose ' + dbURI+ 
-    ' adresindeki veritabanına bağlandı\n');
-});
-//Bağlantı hatası olduğunda konsola hata bilgisini yazdır
-mongoose.connection.on('error',function (err) {
-  console.log('Mongoose bağlantı hatası\n: ' + err);
-});
-//Bağlantı  kesildiğinde konsola kesilme bilgisini yaz.
-mongoose.connection.on('disconnected', function () {
-  console.log('Mongoose bağlantısı kesildi\n');
+var dbURI = 'mongodb://localhost/mekanbul'; // canlıya atacağımız zaman mlab taki adresi yaz buraya
+//var dbURI = 'mongodb://admin:admin@ds115166.mlab.com:15166/mekanbul'
+// mongodb://admin:admin@ds115166.mlab.com:15166/mekanbul  //mlab ile bağlamak içiçn
+//mlab a bağlama işlemi yapmak için.
+//1.)user ve pasword al.
+//2.)heroku config:set --app mekanbul123 MONGOLAB_URI=mongodb://admin:admin@ds115166.mlab.com:15166/mekanbul (admin:admin mlabtaki user ve şifredir.)
+//3.)heroku config:set NODE_ENV=production  --app mekanbul123
+mongoose.connect(dbURI, { 'useMongoClient': true });
+//if (process.env.NODE_ENV === 'production') {  dbURI = process.env.MONGOLAB_URI;} //mlab ile bağlamak içiçn
+
+
+mongoose.connection.on('connected', function (){
+    console.log('\nMongoose\n' + dbURI + '\nadresteki veri tabanına bağlandı\n');
 });
 
-kapat = function(msg, callback) {
-    mongoose.connection.close(function() {
-        console.log('Mongoose kapatıldı\n ' + msg);
+mongoose.connection.on('error', function (err){
+    console.log('\nMongoose bağlanto hatası\n:' + err);
+});
+
+mongoose.connection.on('disconnected', function (){
+    console.log('\nMongoose bağlantısı kesildi\n:');
+});
+
+kapat = function(msg, callback){
+    mongoose.connection.close(function(){
+        console.log('\nMongoose kapatıldı\n' + msg);
         callback();
     });
-};// nodemon kullanıyorsanız ayrı bir kapatma işlemi gerekir.
-process.once('SIGUSR2', function() {
-    kapat('nodemon kapatıldı\n', function() {
+};
+//nodemon kapatma
+process.once('SIGUSR2', function(){
+    kapat('\nnodemon kapatıldı\n', function(){
         process.kill(process.pid, 'SIGUSR2');
     });
-});// Uygulama kapandığında kapat.
-process.on('SIGINT', function() {
-    kapat('Uygulama kapatıldı\n', function() {
-        process.exit(0);
-    });
-});// Herokudan kapatma işlemi gerçekleşirse
-process.on('SIGTERM', function() {
-    kapat('heroku kapatıldı\n', function() {
+});
+//uygulama kapatma
+process.once('SIGINT', function(){
+    kapat('\nUygulama kapatıldı\n', function(){
         process.exit(0);
     });
 });
+//heroku kapatma
+process.once('SIGTERM', function(){
+    kapat('\nheroku kapatıldı\n', function(){
+        process.exit(0);
+    });
+});
+
+
 require('./mekansema'); 
-
-
